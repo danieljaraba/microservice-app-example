66f4082f3c803ac5aedd7e1b89a01a1cda84d136
danieljaraba <daesjaga2003@gmail.com>
2024-04-03 14:11:06 -0500

    Modifying Dockerfiles and k8s implementation

--- auth-api/main.go
+++ auth-api/main.go
@@ -24,7 +24,7 @@ var (
 )
 
 func main() {
-	hostport := ":" + os.Getenv("AUTH_API_PORT")
+	hostport := os.Getenv("AUTH_API_PORT")
 	userAPIAddress := os.Getenv("USERS_API_ADDRESS")
 
 	envJwtSecret := os.Getenv("JWT_SECRET")
